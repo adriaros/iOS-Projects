d9f76ccd9c948081d2e98bc62ab4e30e83adb6f9
Adria Ros Saez <adria.ros.saez@everis.com>
2017-12-13 18:20:06 +0100

    Initiall commit

---
+++ NavigationController/NavigationController/ViewController2.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController2.swift
+//  NavigationController
+//
+//  Created by Adria Ros Saez on 12/13/17.
+//  Copyright © 2017 adriaros. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
